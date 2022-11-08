9b3df8d429eb23069642a02b38abf093ce4fa7fa
madmax484 <111285984+madmax484@users.noreply.github.com>
2022-11-08 21:34:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = '79cab3296ebb60fbaffc832de5b234f8'
-telegram_token = "5742349132:AAEiToGpIk6inkutF9EnbjZy26ez3vo8UbY"
+open_weather_token = 'YOUR_TOKEN'
+telegram_token = "YOUR_TOKEN"
